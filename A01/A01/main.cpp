57ec1e636494937692e1e4dce70997956529623e
alex michalec <alexmc11@hotmail.es>
2017-09-24 18:59:27 +0200

    iteradores vector

--- A01/A01/main.cpp
+++ A01/A01/main.cpp
@@ -6,6 +6,31 @@
 #include <map>
 #include <set>
 
+//FUNCIONES ITERADORES
+
+void iteratorsVector(std::vector<int> v)
+{
+	std::cout << "VECTOR" << std::endl;
+	std::cout << std::endl;
+	typedef std::vector<int>::iterator forit;
+	typedef std::vector<int>::reverse_iterator revit;
+
+	for (forit it = v.begin(); it != v.end(); it++)
+	{
+		std::cout << *it << " ";
+	}
+
+	std::cout << std::endl;
+
+	for (revit rit = v.rbegin(); rit != v.rend(); rit++)
+	{
+		std::cout << *rit << " ";
+	}
+
+	std::cout << std::endl;
+	std::cout << std::endl;
+}
+
 void main()
 {
 	std::vector<int> v1{ 3, 6, 7, 4, 8 };
@@ -14,4 +39,6 @@ void main()
 	std::forward_list<int> fl1{ 4, 5, 1 ,2 ,3 };
 	std::map<char, int> m1;
 	std::set<int> s1;
+
+	iteratorsVector(v1);
 }
