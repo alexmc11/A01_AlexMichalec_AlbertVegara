43af565bd1cce804bd6fd1db386d967ffe7af308
alex michalec <alexmc11@hotmail.es>
2017-09-25 18:16:53 +0200

    List y forward list

--- A01/A01/main.cpp
+++ A01/A01/main.cpp
@@ -12,6 +12,10 @@ typedef std::vector<int>::iterator vforit;
 typedef std::vector<int>::reverse_iterator vrevit;
 typedef std::deque<int>::iterator dforit;
 typedef std::deque<int>::reverse_iterator drevit;
+typedef std::list<int>::iterator lforit;
+typedef std::list<int>::reverse_iterator lrevit;
+typedef std::forward_list<int>::iterator flforit;
+
 
 //FUNCIONES ITERADORES
 
@@ -59,6 +63,43 @@ void iteratorsDeque(std::deque<int> d)
 	std::cout << std::endl;
 }
 
+void iteratorsList(std::list<int> l)
+{
+	std::cout << "LIST" << std::endl;
+	std::cout << std::endl;
+
+
+	for (lforit it = l.begin(); it != l.end(); it++)
+	{
+		std::cout << *it << " ";
+	}
+
+	std::cout << std::endl;
+
+	for (lrevit rit = l.rbegin(); rit != l.rend(); rit++)
+	{
+		std::cout << *rit << " ";
+	}
+
+	std::cout << std::endl;
+	std::cout << std::endl;
+}
+
+void iteratorsForwardList(std::forward_list<int> fl)
+{
+	std::cout << "FORWARD LIST" << std::endl;
+	std::cout << std::endl;
+
+
+	for (flforit it = fl.begin(); it != fl.end(); it++)
+	{
+		std::cout << *it << " ";
+	}
+
+	std::cout << std::endl;
+	std::cout << std::endl;
+}
+
 void main()
 {
 	std::vector<int> v1{ 3, 6, 7, 4, 8 };
@@ -70,4 +111,6 @@ void main()
 
 	iteratorsVector(v1);
 	iteratorsDeque(d1);
+	iteratorsList(l1);
+	iteratorsForwardList(fl1);
 }
