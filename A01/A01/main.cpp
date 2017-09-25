cd4195f2a28b9a9175c52a8e406cb3b8cc42b72d
alex michalec <alexmc11@hotmail.es>
2017-09-25 18:41:13 +0200

    iteradores y constructores por copia hechos

--- A01/A01/main.cpp
+++ A01/A01/main.cpp
@@ -6,7 +6,7 @@
 #include <map>
 #include <set>
 
-//TYPEDEF ITERADORES
+//TYPEDEF ITERADORES///////////////////////////////////////
 
 typedef std::vector<int>::iterator vforit;
 typedef std::vector<int>::reverse_iterator vrevit;
@@ -17,9 +17,11 @@ typedef std::list<int>::reverse_iterator lrevit;
 typedef std::forward_list<int>::iterator flforit;
 typedef std::map<char, int>::iterator mforit;
 typedef std::map<char, int>::reverse_iterator mrevit;
+typedef std::set<int>::iterator sforit;
+typedef std::set<int>::reverse_iterator srevit;
 
 
-//FUNCIONES ITERADORES
+//FUNCIONES ITERADORES////////////////////////////////////
 
 void iteratorsVector(std::vector<int> v)
 {
@@ -124,12 +126,45 @@ void iteratorsMap(std::map<char, int> m)
 	std::cout << std::endl;
 }
 
+void iteratorsSet(std::set<int> s)
+{
+	std::cout << "SET" << std::endl;
+	std::cout << std::endl;
+
+
+	for (sforit it = s.begin(); it != s.end(); it++)
+	{
+		std::cout << *it << " ";
+	}
+
+	std::cout << std::endl;
+
+	for (srevit rit = s.rbegin(); rit != s.rend(); rit++)
+	{
+		std::cout << *rit << " ";
+	}
+
+	std::cout << std::endl;
+	std::cout << std::endl;
+}
+
 void main()
 {
+
+	//CONSTRUCTORES///////////////////////////////////////////
+
 	std::vector<int> v1{ 3, 6, 7, 4, 8 };
+	std::vector<int> v2(v1);
+
 	std::deque<int> d1{ 2, 4, 6, 8, 9 };
+	std::deque<int> d2(d1);
+
 	std::list<int> l1{ 1, 2, 3, 4, 5 };
+	std::list<int> l2(l1);
+
 	std::forward_list<int> fl1{ 4, 5, 1 ,2 ,3 };
+	std::forward_list<int> fl2(fl1);
+
 	std::map<char, int> m1;
 	{
 		m1['a'] = 9;
@@ -137,12 +172,17 @@ void main()
 		m1['c'] = 7;
 		m1['d'] = 6;
 	}
-	std::set<int> s1;
+	std::map<char, int> m2(m1);
 
+	std::set<int> s1{ 1, 4, 9, 7 };
+	std::set<int> s2(s1);
+
+	//LLAMADAS FUNCIONES ITERADORES/////////////////////////
 
 	iteratorsVector(v1);
 	iteratorsDeque(d1);
 	iteratorsList(l1);
 	iteratorsForwardList(fl1);
 	iteratorsMap(m1);
+	iteratorsSet(s1);
 }
