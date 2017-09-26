17a798d5ef41bf6de4bf36a5265c8de8e8467f0e
alex michalec <alexmc11@hotmail.es>
2017-09-26 09:05:01 +0200

    constructores y iteradores constantes

--- A01/A01/main.cpp
+++ A01/A01/main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <vector>
 #include <deque>
+#include <queue>
+#include <stack>
 #include <list>
 #include <forward_list>
 #include <map>
@@ -10,15 +12,26 @@
 
 typedef std::vector<int>::iterator vforit;
 typedef std::vector<int>::reverse_iterator vrevit;
+typedef std::vector<int>::const_iterator cvforit;
+typedef std::vector<int>::const_reverse_iterator cvrevit;
 typedef std::deque<int>::iterator dforit;
 typedef std::deque<int>::reverse_iterator drevit;
+typedef std::deque<int>::const_iterator cdforit;
+typedef std::deque<int>::const_reverse_iterator cdrevit;
 typedef std::list<int>::iterator lforit;
 typedef std::list<int>::reverse_iterator lrevit;
+typedef std::list<int>::const_iterator clforit;
+typedef std::list<int>::const_reverse_iterator clrevit;
 typedef std::forward_list<int>::iterator flforit;
+typedef std::forward_list<int>::const_iterator cflforit;
 typedef std::map<char, int>::iterator mforit;
 typedef std::map<char, int>::reverse_iterator mrevit;
+typedef std::map<char, int>::const_iterator cmforit;
+typedef std::map<char, int>::const_reverse_iterator cmrevit;
 typedef std::set<int>::iterator sforit;
 typedef std::set<int>::reverse_iterator srevit;
+typedef std::set<int>::const_iterator csforit;
+typedef std::set<int>::const_reverse_iterator csrevit;
 
 
 //FUNCIONES ITERADORES////////////////////////////////////
@@ -37,6 +50,18 @@ void iteratorsVector(std::vector<int> v)
 	std::cout << std::endl;
 
 	for (vrevit rit = v.rbegin(); rit != v.rend(); rit++)
+	{
+		std::cout << *rit << " ";
+	}
+
+	for (cvforit it = v.begin(); it != v.end(); it++)
+	{
+		std::cout << *it << " ";
+	}
+
+	std::cout << std::endl;
+
+	for (cvrevit rit = v.rbegin(); rit != v.rend(); rit++)
 	{
 		std::cout << *rit << " ";
 	}
@@ -63,6 +88,18 @@ void iteratorsDeque(std::deque<int> d)
 		std::cout << *rit << " ";
 	}
 
+	for (cdforit it = d.begin(); it != d.end(); it++)
+	{
+		std::cout << *it << " ";
+	}
+
+	std::cout << std::endl;
+
+	for (cdrevit rit = d.rbegin(); rit != d.rend(); rit++)
+	{
+		std::cout << *rit << " ";
+	}
+
 	std::cout << std::endl;
 	std::cout << std::endl;
 }
@@ -85,6 +122,18 @@ void iteratorsList(std::list<int> l)
 		std::cout << *rit << " ";
 	}
 
+	for (clforit it = l.begin(); it != l.end(); it++)
+	{
+		std::cout << *it << " ";
+	}
+
+	std::cout << std::endl;
+
+	for (clrevit rit = l.rbegin(); rit != l.rend(); rit++)
+	{
+		std::cout << *rit << " ";
+	}
+
 	std::cout << std::endl;
 	std::cout << std::endl;
 }
@@ -96,6 +145,11 @@ void iteratorsForwardList(std::forward_list<int> fl)
 
 
 	for (flforit it = fl.begin(); it != fl.end(); it++)
+	{
+		std::cout << *it << " ";
+	}
+
+	for (cflforit it = fl.begin(); it != fl.end(); it++)
 	{
 		std::cout << *it << " ";
 	}
@@ -122,6 +176,18 @@ void iteratorsMap(std::map<char, int> m)
 		std::cout << rit->first << " => " << rit->second << " " << std::endl;
 	}
 
+	for (cmforit it = m.begin(); it != m.end(); it++)
+	{
+		std::cout << it->first << " => " << it->second << " " << std::endl;
+	}
+
+	std::cout << std::endl;
+
+	for (cmrevit rit = m.rbegin(); rit != m.rend(); rit++)
+	{
+		std::cout << rit->first << " => " << rit->second << " " << std::endl;
+	}
+
 	std::cout << std::endl;
 	std::cout << std::endl;
 }
@@ -144,6 +210,18 @@ void iteratorsSet(std::set<int> s)
 		std::cout << *rit << " ";
 	}
 
+	for (csforit it = s.begin(); it != s.end(); it++)
+	{
+		std::cout << *it << " ";
+	}
+
+	std::cout << std::endl;
+
+	for (csrevit rit = s.rbegin(); rit != s.rend(); rit++)
+	{
+		std::cout << *rit << " ";
+	}
+
 	std::cout << std::endl;
 	std::cout << std::endl;
 }
@@ -153,15 +231,28 @@ void main()
 
 	//CONSTRUCTORES///////////////////////////////////////////
 
+	std::vector<int> v0;
 	std::vector<int> v1{ 3, 6, 7, 4, 8 };
 	std::vector<int> v2(v1);
 
+	std::deque<int> d0;
 	std::deque<int> d1{ 2, 4, 6, 8, 9 };
 	std::deque<int> d2(d1);
 
+	std::queue<int> q1;
+	std::queue<int> q2(q1);
+
+	std::priority_queue<int> pq1;
+	std::priority_queue<int> pq2(pq1);
+
+	std::stack<int> st1;
+	std::stack<int> st2(st1);
+
+	std::list<int> l0;
 	std::list<int> l1{ 1, 2, 3, 4, 5 };
 	std::list<int> l2(l1);
 
+	std::forward_list<int> fl0;
 	std::forward_list<int> fl1{ 4, 5, 1 ,2 ,3 };
 	std::forward_list<int> fl2(fl1);
 
@@ -174,6 +265,7 @@ void main()
 	}
 	std::map<char, int> m2(m1);
 
+	std::set<int> s0;
 	std::set<int> s1{ 1, 4, 9, 7 };
 	std::set<int> s2(s1);
 
